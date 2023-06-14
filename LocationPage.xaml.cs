3da8de98f33009fc272bd787a8e52b0a77090bbf
Nejra <121252576+nejrap@users.noreply.github.com>
2023-06-14 17:36:15 +0200

    Add Location

--- LocationPage.xaml.cs
+++ LocationPage.xaml.cs
@@ -6,4 +6,21 @@ public partial class LocationPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    async private void Mapa_Clicked(object sender, EventArgs e)
+    {
+
+        var location = new Location(43.856333, 18.413222);
+        var options = new MapLaunchOptions { Name = "Pharmacy N&N, 71000 Sarajevo" };
+        try
+        {
+            await Map.OpenAsync(location, options);
+        }
+        catch (Exception ex)
+        {
+        }
+
+
+
+
+    }
 }
