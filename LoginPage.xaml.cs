8e438e52295fd3cad8b6ec2732df6fa7c05132b4
Nejra <121252576+nejrap@users.noreply.github.com>
2023-06-25 16:14:26 +0200

    Add About Us

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,12 +1,14 @@
 
+using Microsoft.Maui.ApplicationModel.Communication;
+
 namespace PharC;
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-       
- 
+    public LoginPage()
+    {
+        InitializeComponent();
+
+
     }
 
     private void LoginBtn_Clicked(object sender, EventArgs e)
@@ -16,11 +18,45 @@ public partial class LoginPage : ContentPage
 
     private void ForgotButton_Clicked(object sender, EventArgs e)
     {
-        
+
     }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new SignupPage());
+
+
+        Preferences.Set("KorisnickoIme", KorisnickoIme.Text);
+
+
+        string KorIme = KorisnickoIme.Text;
+        string email = emailEntry.Text;
+       string pass = Password.Text;
+
+
+
+        List<string> RegistrovaniKorisnici = new List<string>();
+        RegistrovaniKorisnici.Add(KorIme);
+        RegistrovaniKorisnici.Add(email);
+        RegistrovaniKorisnici.Add(pass);
+
+        if (KorIme == null && email == null && pass == null)
+        {
+            BezuspjesnaRegistracija.IsVisible = true;
+            UspjesnaRegistracija.IsVisible = false;
+        }
+        else
+        {
+            BezuspjesnaRegistracija.IsVisible = false;
+            UspjesnaRegistracija.IsVisible = true;
+
+            // Prikazivanje poruke na 2 sekunde, a zatim resetiranje vidljivosti labele
+            Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+            {
+                UspjesnaRegistracija.IsVisible = false;
+                return false; // Prekida ponavljanje tajmera
+            });
+
+            Navigation.PushAsync(new SignupPage());
+        }
     }
 }
